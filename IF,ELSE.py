89e617da1a43590c3d320af0802fe37c01a25487
harigesh-kalarikkal <hharigesh@gmail.com>
2026-09-21 06:55:31 +0530

    Add files via upload

---
+++ IF,ELSE.py
@@ -0,0 +1,80 @@
+# a=int(input("enter your age"))
+#
+# if a>=20:
+#     print("you are eligible for admission")
+# else:
+#     print("you are not eligible for admission")
+
+# b=int(input("Enter a number"))
+#
+# if b>0:
+#     print("It's positive")
+# elif b==0:
+#     print("zero")
+# elif b>=-1:
+#     print("NO")
+# elif b>=-2:
+#     print("NO")
+# else:
+#     print("It's negative")
+
+# c=int(input("enter your mark"))
+# if c>100 or c<0:
+#     print(" Invalid mark")
+# elif c>=90:
+#     print("A+")
+# elif c>=80:
+#     print("A")
+# elif c>=70:
+#     print("B+")
+# elif c>=60:
+#     print("B")
+# elif c>=50:
+#     print("C+")
+# elif c>=40:
+#     print("C")
+# else:
+#     print("Fail")
+#
+# a = 14
+#
+# if a % 2 == 0:
+#     print("Even")
+# else:
+#     print("Odd")
+#
+# a = 15
+#
+# if a % 5 == 0:
+#     print("Multiple of 5")
+# else:
+#     print("Not a multiple of 5")
+#
+# a = 5
+# b = 7
+#
+# if a > b:
+#     print("a is larger")
+# else:
+#     print("b is larger")
+
+# a=20
+# b=20
+# c=60
+#
+# if a > b and a > c:
+#     print("a is largest")
+# elif b > a and b > c:
+#     print("b is largest")
+# else:
+#     print("c is largest")
+
+a=15
+
+if a % 3 == 0 and a % 5 == 0:
+    print("Multiple of both 3 and 5")
+else:
+    print("Not a multiple of both 3 and 5")
+
+
+
---
+++ Input.py
@@ -0,0 +1,53 @@
+# firstname=input("firstname")
+# lastname=input("lastname")
+# print("hello",firstname,lastname)
+
+# num1=int(input("first number"))
+# num2=int(input("second number"))
+# print(num1+num2)
+# print(num1-num2)
+# print(num1*num2)
+
+# num1=float(input("first number"))
+# num2=float(input("second number"))
+# print(num1+num2)
+# print(num1-num2)
+
+# x=20
+# y=10
+
+# print(x+y)
+# print(x-y)
+# print(x*y)
+# print(x/y)
+# print(x//y)
+# print(x%y)
+# print(x**y)
+
+# x=6
+# x+=7
+# x-=7
+# print(x)
+
+x=10
+y=20
+#
+# print(x==y)
+# print(x!=y)
+# print(x>y)
+# print(x<y)
+# print(x>=y)
+# print(x<=y)
+
+print(x>y and x>10)
+print(x<y or x<12)
+print(not (x > y and x > 15))
+
+
+
+
+
+
+
+
+
