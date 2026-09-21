959237ae3f5967f267e4c91442b53f43f4634fd8
harigesh-kalarikkal <hharigesh@gmail.com>
2026-09-21 06:53:21 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,22 @@
+# print ("hello world")
+# print ("hi")
+# print (123)
+#
+# x=12345678
+# b="lets not stand on ceremony here"
+# c="  Mr.Wayne"
+
+
+# num=123
+# num=140
+# Num=183
+# nUm="hello"
+# nuM="hy"
+# num1=num
+
+# a=20
+# d=30
+# a,d=d,a
+# _ =455
+# print(c,b,_)
+
